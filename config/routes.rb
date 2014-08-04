e084b718c51137b7c70b00b3297d880ab9c30e4e
bshore01 <benjamin.shore@flatironschool.com>
2014-08-04 14:33:01 -0400

    added wit microphone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/hello'
+
+  root :to => "welcome#hello"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
